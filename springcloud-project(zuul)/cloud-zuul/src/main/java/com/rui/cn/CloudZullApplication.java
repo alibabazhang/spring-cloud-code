3b963e9a8c406d311d60ded80d2bf5cf9652ecb9
zhangruilin <617401579@qq.com>
2018-12-10 14:06:53 +0800

    增加 zuul服务认证模块

--- springcloud-project(zuul)/cloud-zuul/src/main/java/com/rui/cn/CloudZullApplication.java
+++ springcloud-project(zuul)/cloud-zuul/src/main/java/com/rui/cn/CloudZullApplication.java
@@ -2,15 +2,30 @@ package com.rui.cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-public class CloudZullApplication {
+@EnableOAuth2Sso
+public class CloudZullApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudZullApplication.class, args);
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("login", "/cloud-proxy/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable();
     }
 }

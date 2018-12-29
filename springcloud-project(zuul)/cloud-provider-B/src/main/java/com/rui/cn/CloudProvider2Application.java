2156e68cdc8559c465c9f8c00c2c6531837a0644
zhangruilin <617401579@qq.com>
2018-12-29 17:52:11 +0800

    增加 zuul服务认证模块

--- springcloud-project(zuul)/cloud-provider-B/src/main/java/com/rui/cn/CloudProvider2Application.java
+++ springcloud-project(zuul)/cloud-provider-B/src/main/java/com/rui/cn/CloudProvider2Application.java
@@ -4,25 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
 //@EnableResourceServer
-public class CloudProvider2Application /*extends ResourceServerConfigurerAdapter */{
+public class CloudProvider2Application /*extends ResourceServerConfigurerAdapter */ {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudProvider2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProvider2Application.class, args);
+    }
 
 //	@Override
 //	public void configure(HttpSecurity http) throws Exception {
--- springcloud-project(zuul)/cloud-zuul/src/main/java/com/rui/cn/CloudZullApplication.java
+++ springcloud-project(zuul)/cloud-zuul/src/main/java/com/rui/cn/CloudZullApplication.java
@@ -1,39 +1,34 @@
 package com.rui.cn;
 
-import com.rui.cn.filter.SecondPreFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-@EnableOAuth2Sso
-public class CloudZullApplication extends WebSecurityConfigurerAdapter {
+//@EnableOAuth2Sso
+public class CloudZullApplication /*extends WebSecurityConfigurerAdapter */ {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudZullApplication.class, args);
     }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("login", "/cloud-proxy/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .csrf()
-                .disable();
-    }
-    @Bean
-    public SecondPreFilter secondPreFilter(){
-        return new SecondPreFilter();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("login", "/cloud-proxy/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .csrf()
+//                .disable();
+//    }
+//    @Bean
+//    public SecondPreFilter secondPreFilter(){
+//        return new SecondPreFilter();
+//    }
 
 }
